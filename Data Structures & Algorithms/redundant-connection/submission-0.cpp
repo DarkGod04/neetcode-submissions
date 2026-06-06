c87f7db5559dc02b6263ed8bbe3b5107d8696ba1
DarkGod04 <nikhilkumarsingh004@gmail.com>
2026-06-07 01:09:41 +0530

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool dfs(int node,int parent,vector<vector<int>>& adj,vector<bool>& visit){
+        if(visit[node]) return true;//cycle detected
+        visit[node]=true;
+        for(int nei: adj[node]){
+            if(nei == parent) continue;
+            if(dfs(nei,node,adj,visit)) return true;
+        }
+        return false;
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n=edges.size();
+        vector<vector<int>>adj(n+1);
+        for(auto & edge : edges){
+            int u=edge[0], v=edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+            vector<bool>visit(n+1,false);
+            if(dfs(u,-1,adj,visit)) return {u,v};
+        }
+        return {};
+    }
+};
