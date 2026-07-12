69c29d9303d570809571d85feb1dd7df8716222a
DarkGod04 <nikhilkumarsingh004@gmail.com>
2026-07-12 22:26:12 +0530

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(),intervals.end(),[]
+            (const Interval &a , const Interval &b){
+                return a.start<b.start;
+            });
+        int n=intervals.size();
+        int end_time=intervals[0].end;
+        for(int i=1;i<n;i++){
+            if(intervals[i].start<end_time) return false;
+            end_time = intervals[i].end;
+        }
+        return true;
+    }
+};
