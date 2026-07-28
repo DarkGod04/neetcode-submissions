93ffd42fc9f275c20edfefad528a4776fe7211e9
DarkGod04 <nikhilkumarsingh004@gmail.com>
2026-07-28 23:27:38 +0530

    Add: count-paths - submission-2

---
+++ Data Structures & Algorithms/count-paths/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    // int routes(int i,int j,int m,int n,vector<vector<int>>&t){
+    //     if(i==m-1 && j==n-1) return 1;
+    //     if(i<0 || i>=m || j<0 || j>=n) return 0;
+    //     if(t[i][j]!=-1) return t[i][j];
+    //     int right=routes(i,j+1,m,n,t);
+    //     int down=routes(i+1,j,m,n,t);
+    //     return t[i][j]=right+down;
+    // }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>t(m,vector<int>(n));
+        if(m==1 && n==1) return 1;
+        t[0][0]=0;
+        for(int row=1;row<m;row++){
+            t[row][0]=1;
+        }
+        for(int col=1;col<n;col++){
+            t[0][col]=1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                t[i][j]=t[i-1][j]+t[i][j-1];
+            }
+        }
+        return t[m-1][n-1];
+    }
+};
